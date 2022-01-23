51eab5cc9987e2e1a354849164f5509211c5c8b5
Sitaram Ingole <sitaram.ingole@tietoevry.com>
2022-01-23 17:16:00 +0530

    updated changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext ctx =SpringApplication.run(DemoApplication.class, args);
 		
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
